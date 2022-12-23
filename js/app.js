0d18b1037311703713f9e7491c9eba4fab60fcaf
ronymia <mdronymia.javascript.com>
2022-12-23 15:41:21 +0600

    search bug fixed

--- js/app.js
+++ js/app.js
@@ -114,8 +114,8 @@ const updateTotal = () => {
 // search by category
 document.getElementById("search-btn").addEventListener("click", function () {
    const inputField = document.getElementById("input-value").value;
-   const searchedProduct = arr[0].find((p) =>
-     p.category.startsWith(`${inputField}`)
+   const searchedProduct = arr[0].filter((p) =>
+      p.category.includes(inputField.toLowerCase())
    );
    showProducts(searchedProduct);
  });
